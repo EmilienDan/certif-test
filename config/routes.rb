e9280e64cb549c8636bb45d25626a25b5b8dd343
Emilien Danielou <danielou.emilien@gmail.com>
2025-03-31 16:43:29 +0200

    pages

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root to: "pages#home"
+  get "home", to: "pages#home"
   get "about", to: "pages#about"
   get "contact", to: "pages#contact"
 end
